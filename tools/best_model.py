ec3a7191dd4d09aa4dd1e6bbdf5c000ae1f91fca
Vlon Jang <wangqingbaidu@gmail.com>
2017-01-10 16:02:54 +0800

    fix bestmodel.py

--- tools/best_model.py
+++ tools/best_model.py
@@ -8,30 +8,20 @@ From Institute of Computing Technology
 ©2015-2016 All Rights Reserved.
 '''
 import os, sys
+import argparse
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Get best model")
+    parser.add_argument("dir", help='Directory of weights file.', default='.')
+    parser.add_argument('-cfg', help='Configuration of network.', default='./cfg/darknet_ori.cfg')
+    parser.add_argument('-dataset', help='Dataset of network.', default='pnet.dataset')
+    parser.add_argument('-output', help='Output file of validation.', default='validation.log')
+    
+    args = parser.parse_args()
+    
     weights_list = []
-    cfgfile = 'cfg/darknet_es_shortcut_22.cfg'
-    cfgfile = './cfg/darknet_ori_shortcut3.cfg'
-    cfgfile = './cfg/darknet_ori_shortcut_mul.cfg'
-    cfgfile = './cfg/darknet_l_shortcut_mul.cfg'
-    #cfgfile = './cfg/darknet_ori_shortcut_sig.cfg'
-    if len(sys.argv) == 1:
-        flist = os.listdir('.')
-        for f in flist:
-            if '.weights' in f:
-                weights_list.append(f)
-    else:
-        if '-d' in sys.argv:
-            sys.argv = sys.argv[1:]
-            dirpath = sys.argv[sys.argv.index('-d') + 1]
-            sys.argv.remove('-d')
-            sys.argv.remove(dirpath)
-            for i in sys.argv:
-                weights_list.append(i)
-            
-            for i in os.listdir(dirpath):
-                if '.weights' in i:
-                    weights_list.append(dirpath + '/' + i)
+    cfgfile = args.cfg
+    if os.path.exists(args.dir):
+        weights_list = os.system('ls ' + os.path.join(args.dir, '*.weights'))
     
     if len(weights_list) == 0:
         print 'No weights file!'
@@ -40,14 +30,14 @@ if __name__ == '__main__':
         print weights_list
         result = []
         for w in weights_list:
-            print './darknet -i 1 classifier valid cfg/pnet.dataset %s %s' %(cfgfile, w)
-            os.system('./darknet -i 1 classifier valid cfg/pnet_mul.dataset %s %s' %(cfgfile, w))
+            print './darknet -i 1 classifier valid %s %s %s' %(args.dataset, cfgfile, w)
+            os.system('./darknet -i 1 classifier valid %s %s %s' %(args.dataset, cfgfile, w))
             f = open('valid.log').readlines()
             acc = float(f[-2].split()[-1])
             eff = float(f[-1].split()[-2])
             result.append((w, acc, eff))
                           
-        f = open('validation_sig.log', 'w')
+        f = open(args.output, 'w')
         result = sorted(result, key=lambda x:x[1], reverse=1)
         for item in result:
             f.write('weights: %s acc:%f eff:%f\n' %(item[0], item[1], item[2]))
